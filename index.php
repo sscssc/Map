652de30979c13ca99f7fd0386f86f1e3574a0390
sscssc <sscsocom@msn.com>
2014-05-28 13:19:53 +0200

    Map

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Carte SVG des départements français</title>
+</head>
+<body>
+  <div id="canvas_france">
+    <script type="text/javascript" src="csvToArray.v2.1.min.js" charset="utf-8" ></script>
+  	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/mootools/1.2.4/mootools-yui-compressed.js"></script>
+  	<script type="text/javascript" src="raphael-min.js" charset="utf-8" ></script>
+  	<script type="text/javascript" src="map.js" charset="utf-8" ></script>
+  </div>
+
+	  <div id="test" style="
+    position: absolute;
+    top: 5em;
+    left: 40em;">
+	  		<label id="test"></label>
+	  </div>
+</body>
+</html>
